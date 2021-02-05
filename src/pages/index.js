95557c39ef9c5146f7573e3c3bafa4cb3c3895db
tatu-dev-labs <tatu-dev-lab@gmail.com>
2021-02-05 13:10:18 +0100

    Contenido inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,13 @@ import React from "react"
 export default function App() {
 	return (
 		<main>
-
+			<h1>tatu</h1>
+			<h2>Gestión integral de proyectos web</h2>
+			<div>
+				<p>UI/UX</p>
+				<p>Front-End</p>
+				<p>SEO</p>
+			</div>
 		</main>
 	)
 };
