084009936f56fc46e44930e6d3ccd606a76eb465
a.franco <tatu.dev.labs@gmail.com>
2021-02-11 11:40:03 +0100

    .

---
+++ src/components/sections/home-ux.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function HomeUX() {
+	return (
+        <section class="container flex flex-row items-start pb-20 pt-20 space-x-20 w-10/12">
+            <div class="w-1/2">
+                <img src="https://tatu.dev/assets/ui-ux-desktop.svg" title="UX" />
+            </div>
+            <div class="w-1/2">
+                <h2 class="text-4xl font-bold mb-6">UI/UX</h2>
+
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
+
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
+            </div>
+        </section>
+	)
+};
