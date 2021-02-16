7b7c137aa513808163404798b6d910f1fc297c21
a.franco <tatu.dev.labs@gmail.com>
2021-02-16 14:38:48 +0100

    Se atomiza el diseño con clases componentizadas

--- src/components/sections/home-wellcome.js
+++ src/components/sections/home-wellcome.js
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function HomeWellcome() {
 	return (
-        <section className="bl-home-wellcome h-screen w-full bg-primary text-white p-16">
-            <div>
+        <section className="bl-home-wellcome">
+            <div class="co-home-wellcome-container">
                 <h1 className="el-logo-name">tatu</h1>
-                <h2 className="mb-8 text-3xl">Subtitulo</h2>
+                <h2 className="el-home-wellcome-subtitle">Subtitulo</h2>
                 <div>
-                    <p className="text-xl">Item 1, Item 2, Item 3</p>
+                    <p className="el-home-wellcome-text">Item 1, Item 2, Item 3</p>
                 </div>
             </div>
         </section>
