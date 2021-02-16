a4c92b78b2007134e04d407d563d65c11ab2c1bc
a.franco <tatu.dev.labs@gmail.com>
2021-02-16 10:09:05 +0100

    Se reemplaza la clase w-1/2 de las columnas por una clase atómica y reutilizable

--- src/components/sections/home-frontend.js
+++ src/components/sections/home-frontend.js
@@ -5,13 +5,13 @@ import HomeFrontImage from "../../assets/images/front-end-desktop.svg";
 export default function HomeFrontEnd() {
 	return (
         <section className="bl-section-text-image is-reverse">        
-            <div className="w-1/2">
+            <div className="co-section-text-image-col">
                 <h2 className="el-title">Desarrollo Front-End</h2>
 
                 <p className="el-section-text-image-p">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
                 <p className="el-section-text-image-p">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
             </div>
-            <div className="w-1/2">
+            <div className="co-section-text-image-col">
                 <img className="el-section-text-image-img" src={HomeFrontImage} alt="Front-End" />
             </div>
         </section>
