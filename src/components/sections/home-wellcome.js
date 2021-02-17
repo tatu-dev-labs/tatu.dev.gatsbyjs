1e398e00fe95933cdb9974460d8638ac0c747598
a.franco <tatu.dev.labs@gmail.com>
2021-02-17 16:44:29 +0100

    Se elimina subtitulo

--- src/components/sections/home-wellcome.js
+++ src/components/sections/home-wellcome.js
@@ -5,9 +5,8 @@ export default function HomeWellcome() {
         <section className="bl-home-wellcome">
             <div className="co-home-wellcome-container">
                 <h1 className="el-logo-name">tatu</h1>
-                <h2 className="el-home-wellcome-subtitle">Hola, soy Aurelio Franco, gestor de proyectos web. </h2>
                 <div>
-                    <p className="el-home-wellcome-text">Diseñador de <strong>UI/UX</strong>, experto en desarrollo <strong>Front-End</strong> y Optimización <strong>SEO</strong> On Page y SEO Técnico</p>
+                    <p className="el-home-wellcome-text">Diseñador de <strong>UI/UX</strong>, experto en desarrollo <strong>Front-End</strong>, optimización <strong>SEO</strong> On Page y Técnico</p>
                 </div>
             </div>
         </section>
