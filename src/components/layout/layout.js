32e34e14bab3d52e77b9e5a471d15296066cb11e
a.franco <tatu.dev.labs@gmail.com>
2021-02-19 17:13:34 +0100

    Se añaden componentes de cabecera

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,6 @@
 import React from "react"
+import ButtonMenu from "../common/button-menu"
+import MainMenu from "../common/main-menu"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -8,25 +10,29 @@ const iconGithub = <FontAwesomeIcon icon={faGithub} />
 const iconLinkedin = <FontAwesomeIcon icon={faLinkedin} />
 
 export default function Layout({ children }) {
-  return (
-    <div className="pa-layout">
-        <header></header>
-        <main>
-            {children}
-        </main>
-        <footer>
-            <div className="co-footer">
-                <a className="el-footer-icon text-2xl" href="mailto:hola@tatu.dev" target="_blank" rel="noopener noreferrer">
-                  {iconMail}
-                </a>
-                <a className="el-footer-icon text-2xl" href="https://github.com/tatu-dev-labs" target="_blank" rel="noopener noreferrer">
-                  {iconGithub}
-                </a>
-                <a className="el-footer-icon text-2xl" href="https://www.linkedin.com/in/aureliofranco/" target="_blank" rel="noopener noreferrer">
-                  {iconLinkedin}
-                </a>
-            </div>
-        </footer>
-    </div>
-  )
+	
+	return (
+	<div className="pa-layout">		
+		<header>	
+            <MainMenu />	            
+			<ButtonMenu />
+		</header>
+		<main>
+			{children}
+		</main>
+		<footer>
+			<div className="co-footer">
+				<a className="el-footer-icon" href="mailto:hola@tatu.dev" target="_blank" rel="noopener noreferrer">
+					{iconMail}
+				</a>
+				<a className="el-footer-icon" href="https://github.com/tatu-dev-labs" target="_blank" rel="noopener noreferrer">
+					{iconGithub}
+				</a>
+				<a className="el-footer-icon" href="https://www.linkedin.com/in/aureliofranco/" target="_blank" rel="noopener noreferrer">
+					{iconLinkedin}
+				</a>
+			</div>
+		</footer>
+	</div>
+	)
 }
