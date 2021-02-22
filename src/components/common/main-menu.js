04590630225c68ab2bacf1ec8f3a675986682c7b
a.franco <tatu.dev.labs@gmail.com>
2021-02-22 14:58:59 +0100

    Se añaden las opciones de menu de navegacion

--- src/components/common/main-menu.js
+++ src/components/common/main-menu.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import { WebContext } from "../../context/WebContext"
 
 export default function MainMenu() {
@@ -7,7 +8,16 @@ export default function MainMenu() {
         {context => (
             <React.Fragment>
             <div className={context.isMenuVisible ? 'co-main-menu' : 'co-main-menu hidden'}>
-                Menu
+                <nav aria-labelledby="primary-navigation">
+                    <ul>
+                        <li className="el-main-menu-item"><Link to="/" onClick={() => context.toggleMenu()}>Home</Link></li>
+                        <li className="el-main-menu-item"><Link to="/bio/" onClick={() => context.toggleMenu()}>Bio</Link></li>
+                        <li className="el-main-menu-item"><Link to="/ui-ux/" onClick={() => context.toggleMenu()}>UI/UX</Link></li>
+                        <li className="el-main-menu-item"><Link to="/front-end/" onClick={() => context.toggleMenu()}>Front-End</Link></li>
+                        <li className="el-main-menu-item"><Link to="/seo/" onClick={() => context.toggleMenu()}>SEO</Link></li>
+                        <li className="el-main-menu-item"><Link to="/blog/" onClick={() => context.toggleMenu()}>Blog</Link></li>
+                    </ul>
+                </nav>
             </div>
             </React.Fragment>
         )}
