82aeeb7060d645abe41587677491d2a61228a960
a.franco <tatu.dev.labs@gmail.com>
2021-02-23 15:26:52 +0100

    Se añade clase activa

--- src/components/common/main-menu.js
+++ src/components/common/main-menu.js
@@ -8,14 +8,26 @@ export default function MainMenu() {
         {context => (
             <React.Fragment>
             <div className={context.isMenuVisible ? 'co-main-menu' : 'co-main-menu hidden'}>
-                <nav aria-labelledby="primary-navigation">
+                <nav aria-labelledby="primary-navigation" role="navigation">
                     <ul>
-                        <li className="el-main-menu-item"><Link to="/" onClick={() => context.toggleMenu()}>Home</Link></li>
-                        <li className="el-main-menu-item"><Link to="/bio/" onClick={() => context.toggleMenu()}>Bio</Link></li>
-                        <li className="el-main-menu-item"><Link to="/ui-ux/" onClick={() => context.toggleMenu()}>UI/UX</Link></li>
-                        <li className="el-main-menu-item"><Link to="/front-end/" onClick={() => context.toggleMenu()}>Front-End</Link></li>
-                        <li className="el-main-menu-item"><Link to="/seo/" onClick={() => context.toggleMenu()}>SEO</Link></li>
-                        <li className="el-main-menu-item"><Link to="/blog/" onClick={() => context.toggleMenu()}>Blog</Link></li>
+                        <li className="el-main-menu-item">
+                            <Link to="/" activeClassName="active" onClick={() => context.toggleMenu()}>Home</Link>
+                        </li>
+                        <li className="el-main-menu-item">
+                            <Link to="/bio/" activeClassName="active" onClick={() => context.toggleMenu()}>Bio</Link>
+                        </li>
+                        <li className="el-main-menu-item">
+                            <Link to="/ui-ux/" activeClassName="active" onClick={() => context.toggleMenu()}>UI/UX</Link>
+                        </li>
+                        <li className="el-main-menu-item">
+                            <Link to="/front-end/" activeClassName="active" onClick={() => context.toggleMenu()}>Front-End</Link>
+                        </li>
+                        <li className="el-main-menu-item">
+                            <Link to="/seo/" activeClassName="active" onClick={() => context.toggleMenu()}>SEO</Link>
+                        </li>
+                        <li className="el-main-menu-item">
+                            <Link to="/blog/" activeClassName="active" onClick={() => context.toggleMenu()}>Blog</Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
