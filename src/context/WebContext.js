20b52c4d7e3b68ce9ce78f0acee71680ff5a5dca
tatu-dev-labs <tatu-dev-lab@gmail.com>
2021-02-23 23:51:13 +0100

    Se añaden estados para determinar cuando se muestra o no el logo del header

--- src/context/WebContext.js
+++ src/context/WebContext.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 export const WebContext = React.createContext();
 
 const Provider = props => {
-const [isMenuVisible, setMenuVisible] = useState(false);
+const [isMenuVisible, setMenuVisible, isLogoHeaderVisible, setLogoHeaderVisible] = useState(false);
 
 return (
 	<WebContext.Provider value={{
 		isMenuVisible,
-		toggleMenu: () => setMenuVisible(!isMenuVisible)
+		toggleMenu: () => setMenuVisible(!isMenuVisible),
+		isLogoHeaderVisible,
+		showLogoHeader: () => setLogoHeaderVisible(!isMenuVisible)
 	}}>
 	{props.children}
 	</WebContext.Provider>
