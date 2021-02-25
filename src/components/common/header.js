fccab80a8f89d8b652f5b24a388419fde7b05495
a.franco <tatu.dev.labs@gmail.com>
2021-02-25 08:50:51 +0100

    Se añade contexto para utilizar en el scroll y determinar cuando se ve el logo del header y cuando no

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,22 +1,39 @@
 import React, { useContext, useEffect } from "react"
+import { WebContext } from "../../context/WebContext"
 import ButtonMenu from "./button-menu"
 import MainMenu from "./main-menu"
 
+
 export default function Header() {
+    const logoHeaderVisible = useContext(WebContext);
+
     useEffect(() => {
         document.addEventListener('scroll', (e) => {
-            let scrollPosition = window.scrollY;
-            let logoNamePosition = document.getElementsByClassName('el-logo-name')[0].offsetTop;
-            if (scrollPosition >= logoNamePosition) {
-                console.log(true)
+            const currentPath = window.location.pathname;
+            if (currentPath === "/") {
+                const logoNamePosition = document.getElementsByClassName('el-logo-name')[0].offsetTop;
+                const scrollPosition = window.scrollY;
+                if (scrollPosition >= logoNamePosition) {
+                    logoHeaderVisible.toggleLogoHeader(true)
+                } else {
+                    logoHeaderVisible.toggleLogoHeader(false)
+                }
+            } else {
+                logoHeaderVisible.toggleLogoHeader(true)
             }
         })
     })
     return (
         <header className="bl-header">
-        <div className="el-logo-name-small">tatu</div>   
-        <ButtonMenu />
-        <MainMenu />                 
-    </header>
+            <WebContext.Consumer>
+                {context => (
+                    <React.Fragment>
+                    <div className={context.isLogoHeaderVisible ? "el-logo-name-small" : "el-logo-name-small opacity-0" }>tatu</div>   
+                    </React.Fragment>
+                )}        
+            </WebContext.Consumer>
+            <ButtonMenu />
+            <MainMenu />       
+        </header>
     )
 };
