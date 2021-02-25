690fef08916084a7e1d515feaff33cf6159e3be0
a.franco <tatu.dev.labs@gmail.com>
2021-02-25 14:59:41 +0100

    .

---
+++ src/components/sections/hero-text.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function HeroText() {
+	return (
+        <div className="bl-section-hero-text p-28">        
+           <p className="text-2xl text-primary text-center font-light">Internet evoluciona a un ritmo frenético, constantemente hay nuevos retos, nuevas cosas que aprender.<br />Cada día intento mejorar en lo que se y aprender algo de lo que no se.</p>
+        </div>
+	)
+};
