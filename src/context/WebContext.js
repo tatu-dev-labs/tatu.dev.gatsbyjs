da80b5fe83d08ce39650b70629a7f9dc6ec18ae5
a.franco <tatu.dev.labs@gmail.com>
2021-02-25 08:51:45 +0100

    Se añaden las variables y funciones necesarias para manejar estados de logo de cabecera

--- src/context/WebContext.js
+++ src/context/WebContext.js
@@ -3,14 +3,15 @@ import React, { useState } from 'react';
 export const WebContext = React.createContext();
 
 const Provider = props => {
-const [isMenuVisible, setMenuVisible, isLogoHeaderVisible, setLogoHeaderVisible] = useState(false);
+const [isMenuVisible, setMenuVisible] = useState(false);
+const [isLogoHeaderVisible, setLogoHeaderVisible] = useState(false)
 
 return (
 	<WebContext.Provider value={{
 		isMenuVisible,
 		toggleMenu: () => setMenuVisible(!isMenuVisible),
-		isLogoHeaderVisible,
-		showLogoHeader: () => setLogoHeaderVisible(!isMenuVisible)
+        isLogoHeaderVisible,
+        toggleLogoHeader: (value) => setLogoHeaderVisible(value)
 	}}>
 	{props.children}
 	</WebContext.Provider>
