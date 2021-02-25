6b04a8e4a6ba9f9a652fd4dcf7d7399beb19d411
a.franco <tatu.dev.labs@gmail.com>
2021-02-25 08:51:22 +0100

    Se setea en cada click el contexto para el logo de la cabecera

--- src/components/common/main-menu.js
+++ src/components/common/main-menu.js
@@ -11,22 +11,22 @@ export default function MainMenu() {
                 <nav aria-labelledby="primary-navigation" role="navigation">
                     <ul>
                         <li className="el-main-menu-item">
-                            <Link to="/" activeClassName="active" onClick={() => context.toggleMenu()}>Home</Link>
+                            <Link to="/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(false)}}>Home</Link>
                         </li>
                         <li className="el-main-menu-item">
-                            <Link to="/bio/" activeClassName="active" onClick={() => context.toggleMenu()}>Bio</Link>
+                            <Link to="/bio/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(true)}}>Bio</Link>
                         </li>
                         <li className="el-main-menu-item">
-                            <Link to="/ui-ux/" activeClassName="active" onClick={() => context.toggleMenu()}>UI/UX</Link>
+                            <Link to="/ui-ux/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(true)}}>UI/UX</Link>
                         </li>
                         <li className="el-main-menu-item">
-                            <Link to="/front-end/" activeClassName="active" onClick={() => context.toggleMenu()}>Front-End</Link>
+                            <Link to="/front-end/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(true)}}>Front-End</Link>
                         </li>
                         <li className="el-main-menu-item">
-                            <Link to="/seo/" activeClassName="active" onClick={() => context.toggleMenu()}>SEO</Link>
+                            <Link to="/seo/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(true)}}>SEO</Link>
                         </li>
                         <li className="el-main-menu-item">
-                            <Link to="/blog/" activeClassName="active" onClick={() => context.toggleMenu()}>Blog</Link>
+                            <Link to="/blog/" activeClassName="active" onClick={() => {context.toggleMenu(); context.toggleLogoHeader(true)}}>Blog</Link>
                         </li>
                     </ul>
                 </nav>
