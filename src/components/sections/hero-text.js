0b4294976d8502f6b0c831f0292d5cd65d89b7fe
a.franco <tatu.dev.labs@gmail.com>
2021-02-26 14:57:41 +0100

    Se incluye el avatar

--- src/components/sections/hero-text.js
+++ src/components/sections/hero-text.js
@@ -1,9 +1,11 @@
 import React from "react"
+import AvatarImage from "../../assets/images/avatar.png";
 
 export default function HeroText() {
 	return (
-        <div className="bl-section-hero-text p-28">        
-           <p className="text-2xl text-primary text-center font-light">Internet evoluciona a un ritmo frenético, constantemente hay nuevos retos, nuevas cosas que aprender.<br />Cada día intento mejorar en lo que se y aprender algo de lo que no se.</p>
+        <div className="bl-section-hero-text">   
+            <img className="el-avatar" src={AvatarImage} alt="tatu.dev" />
+            <p className="el-hero-text-p">Internet evoluciona a un ritmo frenético, constantemente hay nuevos retos, nuevas cosas que aprender. Cada día intento mejorar en lo que se y aprender algo de lo que no se.</p>
         </div>
 	)
 };
