17d0bd7346a52668066246d92f41b7bd328a70c8
a.franco <tatu.dev.labs@gmail.com>
2021-03-05 16:52:32 +0100

    Se genera contenido inicial conectado a storyblok

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from "react"
-import Layout from "../components/layout/layout";
-import SEOCONF from "../components/seo-conf";
+import { graphql } from "gatsby"
+import Layout from "../components/layout/layout"
+import SEOCONF from "../components/seo-conf"
 
 
-export default function Blog() {
+export default function Blog({data}) {
 	return (
 		<Layout>
 			<SEOCONF />
+            <section className="container flex flex-row items-start flex-wrap w-10/12">
+            {data.allStoryblokEntry.edges.map(({ node }, index) => (
+              <div className="w-1/3 px-14 mb-8" key={index}>
+                  <div className="p-8">
+                    <h2 className="text-xl font-bold">{node.field_title_string}</h2>
+                    <span className="inline-block text-xs mb-4">{node.field_date_string}</span>
+                    <p>{node.field_excerpt_string}</p>
+                  </div>
+              </div>
+            ))}
+            </section>
 		</Layout>
 	)
 };
+
+export const query = graphql`
+query {
+    allStoryblokEntry {
+        edges {
+            node {
+                id
+                field_title_string
+                field_excerpt_string
+                field_date_string
+                slug
+            }
+        }
+    }
+}
+`
